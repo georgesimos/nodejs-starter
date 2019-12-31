624fcacd9e00fdee5a2bafb0742aace0a4f213cb
George <admin@georgesimos.com>
2019-12-31 16:35:04 +0200

    get profile details, update and delete

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const auth = require('../config/auth');
 
 /* Create a user */
-router.post('/users', auth, async (req, res) => {
+router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
@@ -21,6 +21,97 @@ router.get('/users', auth, async (req, res) => {
     res.send(users);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+/* Get logged in user details */
+router.get('/users/me', auth, async (req, res) => {
+  try {
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+/* Get user by id */
+router.get('/users/:id', auth, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    return !user ? res.sendStatus(404) : res.send(user);
+  } catch (e) {
+    return res.sendStatus(400);
+  }
+});
+
+/* Update logged in user */
+router.patch('/users/me', auth, async (req, res) => {
+  try {
+    const { user } = req;
+    user.email = req.body.email || '';
+    user.profile.name = req.body.name || '';
+    user.profile.gender = req.body.gender || '';
+    user.profile.location = req.body.location || '';
+    user.profile.website = req.body.website || '';
+
+    await user.save();
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+/* Update user by id */
+router.patch('/users/:id', auth, async (req, res) => {
+  const validationErrors = [];
+  const updates = Object.keys(req.body);
+  const profileUpdates = ['name', 'gender', 'location', 'website'];
+  const allowedUpdates = ['email', 'password', 'role', ...profileUpdates];
+  const isValidOperation = updates.every(update => {
+    const isValid = allowedUpdates.includes(update);
+    if (!isValid) validationErrors.push(update);
+    return isValid;
+  });
+
+  if (!isValidOperation)
+    return res.status(400).send({ error: `Invalid updates: ${validationErrors.join(',')}` });
+
+  try {
+    const _id = req.params.id;
+    const user = await User.findById(_id);
+    if (!user) return res.sendStatus(404);
+    updates.forEach(update => {
+      if (profileUpdates.includes(update)) user.profile[update] = req.body[update];
+      user[update] = req.body[update];
+    });
+    await user.save();
+
+    return res.send(user);
+  } catch (e) {
+    return res.status(400).send(e);
+  }
+});
+
+/* Delete logged in user */
+router.delete('/users/me', auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send({ message: 'User Deleted' });
+  } catch (e) {
+    res.sendStatus(400);
+  }
+});
+
+/* Delete user by id */
+router.delete('/users/:id', auth, async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findByIdAndDelete(_id);
+    if (!user) return res.sendStatus(404);
+
+    return res.send({ message: 'User Deleted' });
+  } catch (e) {
+    return res.sendStatus(400);
   }
 });
 
@@ -46,6 +137,17 @@ router.post('/users/logout', auth, async (req, res) => {
       return token.token !== req.token;
     });
     await user.save();
+    res.send({ message: 'You have successfully logged out!' });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+/* Logout user from all devices */
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
     res.send({ message: 'You have successfully logged out!' });
   } catch (e) {
     res.status(400).send(e);
