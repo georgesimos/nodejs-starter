09cec38b156ad186ff452bcfe91f45e686ade2ce
George <admin@georgesimos.com>
2019-12-31 13:44:44 +0200

    password hash and finding user by credentials

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const { Schema } = mongoose;
@@ -22,10 +23,21 @@ const userSchema = Schema(
       website: String,
       picture: String,
     },
-    tokens: [{ token: { type: String, required: true } }],
+    tokens: [{ token: { type: String, required: true }, expired: Number }],
   },
   { timestamps: true }
 );
+
+/**
+ * Password hash middleware.
+ */
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 
 /**
  * Hide properties of Mongoose User object.
@@ -48,10 +60,25 @@ userSchema.methods.toJSON = function() {
  */
 userSchema.methods.generateAuthToken = async function() {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
-  user.tokens = user.tokens.concat({ token });
+  const expired = Math.floor(Date.now() / 1000) + 20 * 60;
+  const token = jwt.sign({ _id: user._id.toString(), iat: expired }, process.env.JWT_SECRET);
+  user.tokens = user.tokens.concat({ token, expired });
   await user.save();
-  return token;
+  return { token, expired };
+};
+
+/**
+ * Helper static method for finding user by credentials
+ */
+userSchema.statics.findByCredentials = async function(email, password) {
+  const User = this;
+  const user = await User.findOne({ email });
+  if (!user) throw new Error('Unable to login');
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error('Unable to login');
+
+  return user;
 };
 
 const User = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,23 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
+    const { token, expired } = await user.generateAuthToken();
+    res.status(201).send({ user, token: { token, expired } });
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+// Login User
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const { token, expired } = await user.generateAuthToken();
+    res.send({ user, token: { token, expired } });
+  } catch (e) {
+    res.status(400).send({
+      error: { message: 'You have entered an invalid email or password' },
+    });
   }
 });
 
