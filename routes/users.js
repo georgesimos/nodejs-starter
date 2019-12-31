ce6dee26327bbf3e9f1d8ba2e2cab5c7754e5b12
George <admin@georgesimos.com>
2019-12-31 14:57:35 +0200

    adding logout

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,20 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const auth = require('../config/auth');
 
-// Create a user
+/* Create a user */
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    const { token, expired } = await user.generateAuthToken();
-    res.status(201).send({ user, token: { token, expired } });
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-// Login User
-router.post('/users/login', async (req, res) => {
-  try {
-    const user = await User.findByCredentials(req.body.email, req.body.password);
-    const { token, expired } = await user.generateAuthToken();
-    res.send({ user, token: { token, expired } });
-  } catch (e) {
-    res.status(400).send({
-      error: { message: 'You have entered an invalid email or password' },
-    });
-  }
-});
-
-// Get all users
+/* Get all users */
 router.get('/users', async (req, res) => {
   try {
     const users = await User.find({});
@@ -36,4 +24,31 @@ router.get('/users', async (req, res) => {
   }
 });
 
+/* Login User */
+router.post('/users/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send({
+      error: { message: 'You have entered an invalid email or password' },
+    });
+  }
+});
+
+/* Logout user */
+router.post('/users/logout', auth, async (req, res) => {
+  const { user } = req;
+  try {
+    user.tokens = user.tokens.filter(token => {
+      return token.token !== req.token;
+    });
+    await user.save();
+    res.send({ message: 'You have successfully logged out!' });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 module.exports = router;
