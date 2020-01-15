6be9b653909a3c1d1e5d2301a824e861d24cfc65
George <admin@georgesimos.com>
2020-01-15 19:30:07 +0200

    Tuning index.js

--- index.js
+++ index.js
@@ -12,21 +12,18 @@ const logger = require('morgan');
 const dotenv = require('dotenv');
 const expressStatusMonitor = require('express-status-monitor');
 const connectDB = require('./config/mongoose');
+const routes = require('./routes');
 
-/* Make all variables from our .env file available in our process */
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: '.env.example' });
-}
+// Make all variables from our .env file available in our process
+dotenv.config({ path: '.env.example' });
 
-/* Init express server */
+// Init express server
 const app = express();
 
-/**
- * Connect to MongoDB.
- */
+// Connect to MongoDB.
 connectDB();
 
-/* Middlewares & configs setup */
+// Middlewares & configs setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -38,8 +35,8 @@ app.use((req, res, next) => {
   next();
 });
 
-/* Here we define the api routes */
-app.use('/api/users', require('./routes/api/users'));
+// Here we define the api routes
+app.use(routes);
 
 const port = process.env.PORT || 8080;
 const address = process.env.SERVER_ADDRESS || 'localhost';
